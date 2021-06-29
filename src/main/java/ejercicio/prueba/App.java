6170e56bab1088a10f7070d3e20778c0b4fac12e
Flor21 <fas-2196@hotmail.com>
2021-06-29 07:19:09 -0300

    Modificacion para el front

--- src/main/java/ejercicio/prueba/App.java
+++ src/main/java/ejercicio/prueba/App.java
@@ -1,8 +1,16 @@
 package ejercicio.prueba;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @SpringBootApplication
 public class App 
 {
@@ -11,10 +19,22 @@ public class App
         System.out.println( "Sumar dos numeros enteros cuyo resultado sea mayor a 10." );
     }
     */
+	@RequestMapping("/ ")
+	public String index(Model model) {
+			return "index";
+		}
+	//faltaria llamar al metodo
+	@RequestMapping("/resultadoPeticiones")
+	public String formularioPeticiones(@RequestParam Map<String,String> requestParams, Model modelo) {
+	 String n1 = requestParams.get("nro1");
+	 String n2 = requestParams.get("nro2");
+	 return "resultadoPeticiones";
+	 
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-    public static boolean sumar(Integer A, Integer B)
+    public static boolean sumaMayorADiez(Integer A, Integer B)
     {
     	return ( A + B) >=10?true:false;
     }
